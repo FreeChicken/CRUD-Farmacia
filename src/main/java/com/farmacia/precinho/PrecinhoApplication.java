bf38a19a15c8c0d928c57231de53d7cce0d9d37a
freeChicken <andersonlba.dataco@gmail.com>
2024-06-10 11:40:45 -0300

    Subindo configurações do projeto

---
+++ src/main/java/com/farmacia/precinho/PrecinhoApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.precinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecinhoApplication.class, args);
+	}
+
+}
